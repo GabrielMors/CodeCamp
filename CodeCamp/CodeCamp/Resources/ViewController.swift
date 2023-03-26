58b4d2f2c714662e010d4c5d674d7d5f30bdf69b
Gabriel Mors <gabrielmors210@gmail.com>
2023-03-26 17:27:53 -0400

    setei todos os elementos, coloquei as constraints e coloquei os IBOutlet e o seus valores

--- CodeCamp/CodeCamp/Resources/ViewController.swift
+++ CodeCamp/CodeCamp/Resources/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var senhaLabel: UILabel!
+    @IBOutlet weak var senhaTextField: UITextField!
+    @IBOutlet weak var entrarButton: UIButton!
+    @IBOutlet weak var registrarButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configTextField()
+        configLabel()
     }
 
 
+    private func configTextField() {
+        emailTextField.placeholder = "Digite seu e-mail"
+        senhaTextField.placeholder = "Digite sua senha"
+        
+    }
+    
+    private func configLabel() {
+        emailLabel.text = "E-mail:"
+        senhaLabel.text = "Senha:"
+    }
+    
+    private func configButton() {
+        
+    }
 }
 
